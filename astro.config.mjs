123f9ab1500279f87797d5432d794d1b2a17ca32
sandovaldavid <sandovaldavid2201@gmail.com>
2024-12-26 12:12:52 -0500

    ✨ update astro.config.mjs to include robots.txt integration and set site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+
+import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+  integrations: [tailwind(), robotsTxt()],
+  site: 'https://devsandoval.me/'
+})
