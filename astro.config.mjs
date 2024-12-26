cda12994b0f396ad1586e39c961c1e8cd1d2f1d3
sandovaldavid <sandovaldavid2201@gmail.com>
2024-12-26 12:55:39 -0500

    Update site URL in astro.config.mjs to remove trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://devsandoval.me/'
+  site: 'https://devsandoval.me'
 })
