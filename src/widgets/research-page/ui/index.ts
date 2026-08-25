0e8709ba1dd568720cd7011d4a33ad84d7e9fb5d
David Sandoval <dev@sandovaldavid.com>
2026-08-25 11:13:26 -0500

    feat(research): export evidence composition primitives

--- src/widgets/research-page/ui/index.ts
+++ src/widgets/research-page/ui/index.ts
@@ -1,4 +1,6 @@
 export { default as EvaluationCriteria } from './EvaluationCriteria.astro';
+export { default as ResearchEvidenceColumn } from './ResearchEvidenceColumn.astro';
+export { default as ResearchEvidenceLayout } from './ResearchEvidenceLayout.astro';
 export { default as ResearchPage } from './ResearchPage.astro';
 export { default as ResearchSection } from './ResearchSection.astro';
 export { researchMdxComponents } from './mdx-components';
